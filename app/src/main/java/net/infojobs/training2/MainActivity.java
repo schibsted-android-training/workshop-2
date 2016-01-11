df21ed1a555c5089a9c9b2ef5027b02a3e5ab81d
Rafa Vázquez <rafa.vazsan@gmail.com>
2016-01-11 11:36:06 +0100

    Added some lifecycle callbacks

--- app/src/main/java/net/infojobs/training2/MainActivity.java
+++ app/src/main/java/net/infojobs/training2/MainActivity.java
@@ -2,6 +2,7 @@ package net.infojobs.training2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -18,5 +19,25 @@ public class MainActivity extends AppCompatActivity {
         picasso.setLoggingEnabled(true);
         picasso.load("http://energiaysalud.org/wp-content/uploads/2015/01/benefits-of-eating-banana.jpg")
                 .into(imageView);
+
+        Log.d("IJ", "onCreate!");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("IJ", "onResume :D");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v("IJ", "onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w("IJ", "onDestroy");
     }
 }
