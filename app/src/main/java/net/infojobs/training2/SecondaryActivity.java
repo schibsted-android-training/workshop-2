cf1140bfe62d05b958a99ecee423e2cdf352b50b
Rafa Vázquez <rafa.vazsan@gmail.com>
2016-01-11 13:46:37 +0100

    Created secondary activity

---
+++ app/src/main/java/net/infojobs/training2/SecondaryActivity.java
@@ -0,0 +1,14 @@
+package net.infojobs.training2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SecondaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondary);
+    }
+
+}
